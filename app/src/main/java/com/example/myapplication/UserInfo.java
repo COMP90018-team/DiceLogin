6a0b91cf620788485c3e0443972f81e3b77f421f
Yanbo Wang <yanbow3@student.unimelb.edu.au>
2022-10-27 16:11:47 +0800

    add UserInfo class back

---
+++ app/src/main/java/com/example/myapplication/UserInfo.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+public class UserInfo {
+
+    public String username;
+    public String email;
+    public UserInfo(){}
+    public UserInfo(String username, String email){
+        this.username = username;
+        this.email = email;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+
+}
